49a17a7e2fa63340222b5d7dd63de3c745fc1092
Alex Nichol <unixpickle@gmail.com>
2017-07-31 20:37:47 -0400

    minor attempts at improving knightower.py

--- knightower.py
+++ knightower.py
@@ -1,12 +1,11 @@
 """
 Train an agent on Knightower-v0 in µniverse.
 
-Uses policy gradients with a discount factor of 0.
+Uses policy gradients with no discount factor.
 
 Currently does not work very well.
 """
 
-import math
 import random
 import threading
 
@@ -17,11 +16,11 @@ def main():
     """
     Train an agent.
     """
-    policy_in = tf.placeholder(tf.float32, [None, 480, 320, 3])
+    policy_in = tf.placeholder(tf.float32, [None, 120, 80, 1])
     policy_out = policy(policy_in)
     actions_in, rewards_in, objective = surrogate_objective(policy_out)
 
-    adam = tf.train.AdamOptimizer(learning_rate=1e-2)
+    adam = tf.train.AdamOptimizer(learning_rate=1e-5)
     minimize = adam.minimize(-objective)
 
     with tf.Session() as sess:
@@ -29,6 +28,8 @@ def main():
         roller = Roller(sess, policy_in, policy_out)
         while True:
             obses, actions, rewards, mean = roller.rollouts()
+            print(actions)
+            print(rewards)
             print('mean_reward=%f' % (mean,))
             inputs = {
                 policy_in: obses,
@@ -43,10 +44,9 @@ def policy(in_frames):
     The output is a distribution with two outcomes.
     """
     batch_size = tf.shape(in_frames)[0]
-    fan_in = 320 * 480 * 3
-    flat_in = tf.reshape(in_frames, [batch_size, fan_in])
-    transformation = tf.Variable(tf.random_normal([fan_in, 2],
-                                                  stddev=1/math.sqrt(fan_in)))
+    fan_in = 80 * 120
+    flat_in = tf.reshape(in_frames, [batch_size, fan_in]) / 255
+    transformation = tf.Variable(tf.zeros([fan_in, 2]))
     return tf.nn.softmax(tf.matmul(flat_in, transformation))
 
 def surrogate_objective(policy_out):
@@ -79,7 +79,8 @@ class Roller:
         """
         Collect the given number of rollouts.
 
-        Returns observations, actions, rewards, mean_reward.
+        Returns observations, actions, goodnesses,
+        and mean_reward.
 
         It is not safe to call this from multiple threads
         at a single time on one Roller.
@@ -113,7 +114,8 @@ class Roller:
                 obses, actions, rewards = self._rollout(env)
                 self.lock.acquire()
                 self.total_rewards.append(sum(rewards))
-                self.rewards.extend([[x-0.5] for x in rewards])
+                total = sum(rewards)
+                self.rewards.extend([[total] for _ in rewards])
                 self.obses.extend(obses)
                 self.actions.extend(actions)
                 self.lock.release()
@@ -125,12 +127,13 @@ class Roller:
         obses = []
         actions = []
         env.reset()
-        obs = env.observe()
+        obs = downsample_obs(env.observe())
         while True:
             obses.append(obs)
             out_dist = self.sess.run(self.policy_out,
                                      feed_dict={self.policy_in: [obs]})
             action = [1, 0]
+            print(out_dist)
             key_action = muniverse.key_for_code('ArrowLeft')
             if random.random() < out_dist[0][1]:
                 key_action = muniverse.key_for_code('ArrowRight')
@@ -139,9 +142,15 @@ class Roller:
                                     key_action.with_event('keyUp'))
             rewards.append(reward)
             actions.append(action)
-            obs = env.observe()
+            obs = downsample_obs(env.observe())
             if done:
                 break
         return obses, actions, rewards
+
+def downsample_obs(obs):
+    """
+    Scale down a muniverse observation.
+    """
+    return obs[::4, ::4, 2:3]
 
 main()
